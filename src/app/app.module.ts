988d49a518532af27ebcbeb91f1e3098100db825
unknown <mennaramadan569@gmail.com>
2022-02-23 01:33:22 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CarouselComponent } from './content/carousel/carousel.component';
+import { CaregoryListingComponent } from './content/caregory-listing/caregory-listing.component';
+import { ProductListingComponent } from './content/product-listing/product-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent,
+    CaregoryListingComponent,
+    ProductListingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/content/caregory-listing/caregory-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caregory-listing',
+  templateUrl: './caregory-listing.component.html',
+  styleUrls: ['./caregory-listing.component.css']
+})
+export class CaregoryListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
