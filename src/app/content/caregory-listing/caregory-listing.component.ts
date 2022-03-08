ca7ba34514c3a5c87b1360d21696f5526aa4394a
unknown <mennaramadan569@gmail.com>
2022-03-09 01:30:20 +0200

    updates

--- src/app/content/caregory-listing/caregory-listing.component.ts
+++ src/app/content/caregory-listing/caregory-listing.component.ts
@@ -1,6 +1,7 @@
 import { Category } from './../../_models/category.models';
 import { CategoryServiceService } from './../../services/category-service.service';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-caregory-listing',
@@ -34,4 +35,27 @@ this.service.getAllcategories().subscribe(
   //   console.log('done');
   // });
 
+
+  successNotification() {
+    Swal.fire('Hi', 'We have been informed!', 'success');
+  }
+
+  alertConfirmation() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This process is irreversible.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, go ahead.',
+      cancelButtonText: 'No, let me think',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Removed!', 'Product removed successfully.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Cancelled', 'Product still in our database.)', 'error');
+      }
+    });
+  }
 }
+
+// }
