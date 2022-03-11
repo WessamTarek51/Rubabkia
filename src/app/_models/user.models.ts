5289f0d2af4744a9a687c29d29e31c6a876f6c38
Hemmat <mmhemat99@gmail.com>
2022-03-11 17:11:23 +0200

    sales products

--- src/app/_models/user.models.ts
+++ src/app/_models/user.models.ts
@@ -1,5 +1,6 @@
 import { Purchase } from './purchase.models';
 import { Product } from "./product.models";
+import { Sales } from "./sales.model";
 
 export interface User{
 
@@ -15,12 +16,13 @@ export interface User{
     password:string;
     products?:Product[];
     purchases?:Purchase[];
+    sales?:Sales[];
 
     // email:string;
     // phone_number:number;
     // address:string;
     // password:string;
-   
+
 
 }
 
