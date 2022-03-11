9d25bf718f6987fa5313c3400264d9e8f6f8488e
basmasamir469 <basmaelazony@gmail.com>
2022-03-12 01:41:05 +0200

    add counter to notifications

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserServicesService } from 'src/app/services/user-services.service';
+import { Notifi } from 'src/app/_models/notiication.models';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private router:Router) { }
+  seller=parseInt(localStorage.getItem('user_id')!);
+  notifi!:Notifi[];
+  counter!:number;
+  constructor(private router:Router,private service:UserServicesService) { }
    token:any
    senderID = parseInt(localStorage.getItem('user_id')!)
 
   ngOnInit(): void {
+    this.service.refreshNeeded.subscribe(()=>{
+      this.requset(this.seller)
+    })
+   
+  }
+  requset(seller:number){
+    this.service.request(seller).subscribe(
+      (res)=>{
+
+      this.notifi=res.data
+      console.log(res.data)
+      this.counter=this.notifi.length
+      
+     },)
+     
   }
 logout(){
 
